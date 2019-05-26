430141e6f9e93c1b2d5e049443cec1018ee3919a
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-05-27 00:28:24 +0900

    若干高速化

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 fn main() {
     let max = 1000;
-    // let limit = (max as f32).sqrt() as usize;
+    let limit = (max as f32).sqrt() as usize + 1;
     let mut table = vec![0; max];
     for i in 1..(max-1) {
         table[i] = i;
     }
-    let mut table2 = table.clone();
-    for i in 2..(max-1) {
+    for i in 2..limit {
         let mut j = i;
         while max > j + i {
-            table2[j + i] = 0;
+            table[j + i] = 0;
             j += i;
         }
     }
-    
     for i in 1..max {
-        if table2[i] != 0 {
+        if table[i] != 0 {
             print!("{} ", i);
         }
     }
-    
 }
