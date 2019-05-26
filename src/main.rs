4a01c338996e7c20d7fde31b9e1aabda96438a8d
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-05-27 00:24:31 +0900

    エラトステネスの篩

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let max = 1000;
+    // let limit = (max as f32).sqrt() as usize;
+    let mut table = vec![0; max];
+    for i in 1..(max-1) {
+        table[i] = i;
+    }
+    let mut table2 = table.clone();
+    for i in 2..(max-1) {
+        let mut j = i;
+        while max > j + i {
+            table2[j + i] = 0;
+            j += i;
+        }
+    }
+    
+    for i in 1..max {
+        if table2[i] != 0 {
+            print!("{} ", i);
+        }
+    }
+    
 }
