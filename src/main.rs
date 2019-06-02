6fcc49992dffe39e4dab0c736d2d11804fea7ba8
masanao.matsubara <m_matsubara@okwave.co.jp>
2019-06-03 00:02:14 +0900

    parallel

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 #![feature(test)]
 extern crate test;
 
-fn eratosthenes(max: usize) -> usize {
-    let limit = (max as f32).sqrt().floor() as usize;
-    let mut table = vec![0; max];
-    for i in 1..(max-1) { table[i] = i }
-    for i in 2..limit {
+use std::ops::Range;
+
+fn sift_table(max: usize, table: &mut Vec<usize>, range: Range<usize>) {
+    for i in range {
         let mut j = i;
         while max > j + i {
             table[j + i] = 0;
             j += i;
         }
     }
+}
+
+fn eratosthenes(max: usize) -> usize {
+    let limit = (max as f32).sqrt().floor() as usize;
+    let mut table = vec![0; max];
+    for i in 1..(max-1) { table[i] = i }
+    
+    sift_table(max, &mut table, 2..limit);
+
     let results = table.iter().filter(|i| **i != 0 as usize);
     return results.count();
 }
